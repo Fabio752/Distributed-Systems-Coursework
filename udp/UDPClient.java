6c21a92621aeaef084de664d4f5e0d3d64f327a5
Marco Selvatici <marcoselvatici98@gmail.com>
2019-01-30 15:33:17 +0000

    implement udp client and server. No end to end tests yet

--- udp/UDPClient.java
+++ udp/UDPClient.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
@@ -18,44 +19,64 @@ public class UDPClient {
 
 	public static void main(String[] args) {
 		InetAddress	serverAddr = null;
-		int			recvPort;
-		int 		countTo;
-		String 		message;
+		int	recvPort;
+		int countTo;
+		String message;
 
-		// Get the parameters
+		// Get the parameters.
 		if (args.length < 3) {
-			System.err.println("Arguments required: server name/IP, recv port, message count");
+			System.err.println(
+				"Arguments required: server name/IP, recv port, message count");
 			System.exit(-1);
 		}
 
 		try {
 			serverAddr = InetAddress.getByName(args[0]);
 		} catch (UnknownHostException e) {
-			System.out.println("Bad server address in UDPClient, " + args[0] + " caused an unknown host exception " + e);
+			System.out.println(
+				"Bad server address in UDPClient, " + args[0] +
+				" caused an unknown host exception " + e);
 			System.exit(-1);
 		}
 		recvPort = Integer.parseInt(args[1]);
 		countTo = Integer.parseInt(args[2]);
 
-
-		// TO-DO: Construct UDP client class and try to send messages
+		// Construct UDP client class and try to send messages.
+		UDPClient udpClient = new UDPClient();
+		udpClient.testLoop(serverAddr, recvPort, countTo);
 	}
 
 	public UDPClient() {
-		// TO-DO: Initialise the UDP socket for sending data
+		// Initialise the UDP socket for sending data.
+		try {
+			// Automatically select the port for the socket.
+			sendSoc = new DatagramSocket();
+		} catch(SocketException e) {
+			System.out.println("Socket exception: " + e.getMessage());
+			System.exit(-1);
+		}
 	}
 
-	private void testLoop(InetAddress serverAddr, int recvPort, int countTo) {
-		int				tries = 0;
-
-		// TO-DO: Send the messages to the server
+	private void testLoop(InetAddress serverAddr, int recvPort,
+												int countTo) {
+		// Send the messages to the server.
+		for (int tries = 0; tries < countTo; tries++) {
+			String payload = countTo + ";" + tries;
+			send(payload, serverAddr, recvPort);
+		}
 	}
 
-	private void send(String payload, InetAddress destAddr, int destPort) {
-		int				payloadSize;
-		byte[]				pktData;
-		DatagramPacket		pkt;
-
-		// TO-DO: build the datagram packet and send it to the server
+	private void send(String payload, InetAddress destAddr,
+										int destPort) {
+		// Build the datagram packet and send it to the server.
+		int payloadSize = payload.length();
+		byte[] pktData = payload.getBytes();
+		DatagramPacket pkt = new DatagramPacket(pktData, payloadSize, 
+																						destAddr, destPort);
+		try {
+			sendSoc.send(pkt);
+		} catch(IOException e) {
+			System.out.println("Send exception: " + e.getMessage());
+		}
 	}
 }
--- udp/UDPServer.java
+++ udp/UDPServer.java
@@ -14,55 +14,102 @@ import common.MessageInfo;
 
 public class UDPServer {
 
+	// Timeout to receive a message, in milliseconds.
+	private final static int timeout = 30000;
+
 	private DatagramSocket recvSoc;
 	private int totalMessages = -1;
 	private int[] receivedMessages;
 	private boolean close;
 
 	private void run() {
-		int				pacSize;
-		byte[]			pacData;
-		DatagramPacket 	pac;
+		int	pacSize = 1024;
+		byte[] pacData = new byte[pacSize];
+		DatagramPacket pac;
 
-		// TO-DO: Receive the messages and process them by calling processMessage(...).
-		//        Use a timeout (e.g. 30 secs) to ensure the program doesn't block forever
-
+		// Receive the messages and process them.
+		// Timeout to ensure the program doesn't block forever.
+		close = false;
+		while(!close) {
+			pac = new DatagramPacket(pacData, pacSize);
+			try {
+				recvSoc.setSoTimeout(timeout);
+				recvSoc.receive(pac);
+			} catch (SocketTimeoutException e) {
+				System.out.println("Socket timeout exception: " +
+													 e.getMessage());
+				System.out.println("Quitting server.");
+				System.exit(-1);
+			} catch (IOException e) {
+				System.out.println("Socket IOException: " +
+													 e.getMessage());
+				System.out.println("Quitting server.");
+				System.exit(-1);
+			}
+			processMessage(new String(pac.getData()));
+		}
 	}
 
 	public void processMessage(String data) {
-
 		MessageInfo msg = null;
+		
+		// Use the data to construct a new MessageInfo object.
+		try {
+			msg = new MessageInfo(data);
+		} catch (Exception e) {
+			System.out.println("Message exception: " + e.getMessage());
+		}
 
-		// TO-DO: Use the data to construct a new MessageInfo object
+		// On receipt of first message, initialise the receive buffer
+		// and update the totalMessages count.
+		if (receivedMessages.length == 0) {
+			totalMessages = msg.totalMessages;
+			receivedMessages = new int[totalMessages];
+			System.out.println("New set of message with " + totalMessages +
+												 " messages.");
+		}
 
-		// TO-DO: On receipt of first message, initialise the receive buffer
+		receivedMessages[msg.messageNum] = 1;
+		System.out.println(msg.toString());
 
-		// TO-DO: Log receipt of the message
-
-		// TO-DO: If this is the last expected message, then identify
-		//        any missing messages
-
+		// If this is the last expected message, then identify any
+		// missing messages.
+		if (msg.messageNum == totalMessages - 1) {
+			close = true;
+			for (int i = 0; i < receivedMessages.length; i++ ) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
 	}
 
-
 	public UDPServer(int rp) {
-		// TO-DO: Initialise UDP socket for receiving data
+		// Initialise UDP socket for receiving data.
+		// Use port number passed from cmd.
+		try {
+			recvSoc = new DatagramSocket(rp);
+		} catch (SocketException e) {
+			System.out.println("Socket exception: " + e.getMessage());
+			System.exit(-1);
+		}
 
-		// Done Initialisation
+		// Done Initialisation.
 		System.out.println("UDPServer ready");
 	}
 
 	public static void main(String args[]) {
 		int	recvPort;
 
-		// Get the parameters from command line
+		// Get the parameters from command line.
 		if (args.length < 1) {
 			System.err.println("Arguments required: recv port");
 			System.exit(-1);
 		}
 		recvPort = Integer.parseInt(args[0]);
 
-		// TO-DO: Construct Server object and start it by calling run().
+		// Construct Server object and start it.
+		UDPServer udpServer = new UDPServer(recvPort);
+		udpServer.run();
 	}
 
 }
